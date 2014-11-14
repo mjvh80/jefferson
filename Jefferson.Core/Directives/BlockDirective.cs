ea31fc988ce7f87220c8619db8cde198b184d0bd
Marcus van Houdt <mjvh80@gmail.com>
2014-11-14 09:46:42 +0100

    Add the Jefferson.Core/Directives/BlockDirective.cs#blockJefferson.Core/Directives/BlockDirective.cs directive which does nothing but create a block or in otherwords create a new scope.

---
+++ Jefferson.Core/Directives/BlockDirective.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jefferson.Directives
+{
+   /// <summary>
+   /// Does nothing but copy the current context as a new scope.
+   /// This is not as useless as it seems, e.g. consider
+   /// $$#block$$
+   ///   $$#let x = 'blah'$$
+   ///   $$ $1.x $$
+   /// $$/block$$
+   /// It would otherwise be impossible to access x in the current scope as let does not introduce a new scope.
+   /// </summary>
+   public class BlockDirective: IDirective
+   {
+      public String Name
+      {
+         get { return "block"; }
+      }
+
+      public String[] ReservedWords
+      {
+         get { return null; }
+      }
+
+      public System.Linq.Expressions.Expression Compile(Parsing.TemplateParserContext parserContext, String arguments, String source)
+      {
+         var currentContext = parserContext.GetNthContext(0);
+         var currentContextAsObj = Expression.Convert(currentContext, typeof(Object));
+         parserContext.PushScope(currentContext.Type);
+         var result = Expression.Block(
+                         Expression.Call(parserContext.RuntimeContexts, Utils.GetMethod<List<Object>>(l => l.Add(null)), currentContextAsObj),
+                         Expression.Invoke(parserContext.Parse<Object>(source), currentContextAsObj, parserContext.Output),
+                         Expression.Call(parserContext.RuntimeContexts, Utils.GetMethod<List<Object>>(l => l.RemoveAt(0)),
+                            Expression.Subtract(Expression.Property(parserContext.RuntimeContexts, "Count"), Expression.Constant(1))));
+         parserContext.PopScope();
+         return result;
+      }
+   }
+}
