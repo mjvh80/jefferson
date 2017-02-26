c45710cc04f3a9be1ccf5f03265a4659709cb3c5
Marcus van Houdt <marcus.vanhoudt@lundalogik.se>
2017-02-26 13:50:46 +0100

    Support optional file includes using a ? prefix in the script resolver.

--- Jefferson.Build/MSBuild/ScriptResolver.cs
+++ Jefferson.Build/MSBuild/ScriptResolver.cs
@@ -32,20 +32,35 @@ namespace Jefferson.Build.MSBuild
 
         public override String ResolveReference(String path, String baseFilePath)
         {
+            var isOptional = path.StartsWith("?");
+
+            if (isOptional && path.Length > 1)
+                path = path.Substring(1);
+
             if (Path.IsPathRooted(path))
             {
                 _mTask._LogDiagnostic($"Path {path} referenced from {baseFilePath} resolved as {path}");
-                return path;
+                return isOptional ? $"?{path}" : path;
             }
 
             var parentDirectory = Path.GetFullPath(Path.Combine(baseFilePath, ".."));
             var result = Path.Combine(parentDirectory, path);
-            _mTask._LogDiagnostic($"Path {path} referenced from {baseFilePath} resolved to {result}.");
-            return result;
+            _mTask._LogDiagnostic($"Path {(isOptional ? "?" : "")}{path} referenced from {baseFilePath} resolved to {result}.");
+            return isOptional ? $"?{result}" : result;
         }
 
         public override Stream OpenRead(String resolvedPath)
         {
+            if (resolvedPath.StartsWith("?") && resolvedPath.Length > 1)
+            {
+                var file = resolvedPath.Substring(1);
+                if (File.Exists(file)) return File.OpenRead(file);
+
+                // File not found, return empty.
+                _mTask._Log($"Optional include file {resolvedPath} not found. Ignoring.");
+                return new MemoryStream(0);
+            }
+
             return File.OpenRead(resolvedPath);
         }
     }
